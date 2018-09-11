8a41d88797a24ae9a8c4d1509058082ba78bf43f
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-11 07:04:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,6 @@
 def my_select(collection)
   ret = []
-  (0...collection.size).each_with_index do |item, i|
+  collection.each_with_index do |item, i|
     ret.push(item) if yield item
   end
   ret
